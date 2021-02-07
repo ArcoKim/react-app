5bb5de28441cf2c1ff379b95370e7977c98156cf
Kimjeongtae <mcblueboyk@naver.com>
2021-02-08 00:09:16 +0900

    210207 edit!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react';
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
